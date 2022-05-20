e85977730ec4011aa5062245b712104314609957
unknown <sakintola351@gmail.com>
2022-05-21 00:29:23 +0100

    feat: Add A middleware that targets my route three endpoints

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const availability = require("./routers/availability")
 const port = process.env.PORT || 3000
+
+
+app.use(availability)
 
 app.get('/', (req, res) => {
   res.send('Lumiqualis Take Home Landing Test Page')
