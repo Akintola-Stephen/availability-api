0f6b2bb9aa8379f86c6649496e31d94014884541
unknown <sakintola351@gmail.com>
2022-05-23 12:47:22 +0100

    feat: Add A function in availability route  that returns valid weekend dates based on a supplied parameter

--- routers/availability.js
+++ routers/availability.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
+const express = require("express");
+const app = express();
+const router = express.Router();
+const axios = require("axios");
 
+const weekEnds = (year) => {
+  var date = new Date(year, 0, 1);
+  while (date.getDay() != 0) {
+    date.setDate(date.getDate() + 1);
+  }
+  var days = [];
+  while (date.getFullYear() == year) {
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    days.push(
+      year + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d)
+    );
+    date.setDate(date.getDate() + 7);
+  }
+  return days;
+};
 
+router.get("/country_list", async (req, res) => {
+  try {
+    const country_list = await axios.get(
+      "https://date.nager.at/api/v3/AvailableCountries"
+    );
+    res.send(country_list.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-router.get('/country_list', (req,res)=>{
-    res.send('List of available countries')
-})
+router.get("/local_holidays", (req, res) => {
+  res.send("Available local holidays based on country");
+});
 
+// Endpoint showing list of available times based on his country, from and to
+router.get("/filtered_available_meeting_date", (req, res) => {
+  res.send(
+    "Available meeting dates based on end user country , set from and to date"
+  );
+});
 
-router.get('/local_holidays', (req,res)=>{
-    res.send('Available local holidays based on country')
-})
-
-
-// Endpoint showing list of available times based on his country, from and to 
-router.get('/filtered_available_meeting_date', (req,res)=>{
-    res.send('Available meeting dates based on end user country , set from and to date')
-})
-
-
-module.exports = router
-
+module.exports = router;
