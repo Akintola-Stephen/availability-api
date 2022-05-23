5b342a14817cc2d5ce9b090474b33c06343f7d8d
unknown <sakintola351@gmail.com>
2022-05-23 14:45:53 +0100

    feat: Add A function (getWorkingDateArray) that retuens an array consisting of working dates

--- routers/availability.js
+++ routers/availability.js
@@ -5,12 +5,12 @@ const axios = require("axios");
 
 /**
  * this will return an array containing all the
- * dates between start date and an end date
+ * dates between startDate date and an endDate date
  */
-var getDateArray =  (start, end) => {
-  var arr = new Array();
-  var dt = new Date(start);
-  while (dt <= end) {
+let getDateArray =  (startDate, endDate) => {
+  let arr = new Array();
+  let dt = new Date(startDate);
+  while (dt <= endDate) {
     arr.push(new Date(dt).toString().substring(0, 15)); //saving only to the format Day MMM DD YYYY part
     dt.setDate(dt.getDate() + 1);
   }
@@ -18,42 +18,77 @@ var getDateArray =  (start, end) => {
 };
 
 
-const weekEnds = (year) => {
-  var date = new Date(year, 0, 1);
-  while (date.getDay() != 0) {
-    date.setDate(date.getDate() + 1);
-  }
-  var days = [];
-  while (date.getFullYear() == year) {
-    var m = date.getMonth() + 1;
-    var d = date.getDate();
-    days.push(
-      year + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d)
-    );
-    date.setDate(date.getDate() + 7);
-  }
-  return days;
+/**
+ * this will return an array consisting of the
+ * working dates
+ */
+ let getWorkingDateArray = (dates, hoildayDates, workingWeekendDates) => {
+  // remove holidays
+  let arr = dates.filter((dt) => {
+    return holidaysArray.indexOf(dt) < 0;
+  });
+
+  // remove weekendDate dates that are not working dates
+  let result = arr.filter((dt) => {
+    if (dt.indexOf("Sat") > -1 || dt.indexOf("Sun") > -1) {
+      if (workingWeekendDates.indexOf(dt) > -1) {
+        return dt;
+      }
+    } else {
+      return dt;
+    }
+  });
+
+  return result;
 };
+
+
+
+// const weekendDates = (year) => {
+//   let date = new Date(year, 0, 1);
+//   while (date.getDay() != 0) {
+//     date.setDate(date.getDate() + 1);
+//   }
+//   let days = [];
+//   while (date.getFullYear() == year) {
+//     let m = date.getMonth() + 1;
+//     let d = date.getDate();
+//     days.push(
+//       year + "-" + (m < 10 ? "0" + m : m) + "-" + (d < 10 ? "0" + d : d)
+//     );
+//     date.setDate(date.getDate() + 7);
+//   }
+//   return days;
+// };
 
 router.get("/country_list", async (req, res) => {
   try {
     const country_list = await axios.get(
       "https://date.nager.at/api/v3/AvailableCountries"
     );
-    res.send(country_list.data);
+    console.log(country_list)
+    res.sendDate(country_list.data);
   } catch (error) {
     console.log(error.message);
   }
 });
 
-router.get("/local_holidays", (req, res) => {
-  res.send("Available local holidays based on country");
+router.get("/local_holidays", async(req, res) => {
+  try {
+    const country_list = await axios.get(
+      "https://support.google.com/business/answer/6333474?hl=en#region_ad"
+    );
+    res.sendDate(country_list.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+  res.sendDate("Available local holidays based on country");
 });
 
-// Endpoint showing list of available times based on his country, from and to
+// endDatepoint showing list of available times based on his country, from and to
 router.get("/filtered_available_meeting_date", (req, res) => {
-  res.send(
-    "Available meeting dates based on end user country , set from and to date"
+  res.sendDate(
+    "Available meeting dates based on endDate user country , set from and to date"
   );
 });
 
